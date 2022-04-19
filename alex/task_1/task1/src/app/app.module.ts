d8340bc1d44073f006d841a367aa9c612bd821f5
Alex Pavlov <a.pavlov@ubiquedigital.com>
2022-04-19 17:19:02 +0300

    base routing added

--- alex/task_1/task1/src/app/app.module.ts
+++ alex/task_1/task1/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LayoutComponent } from './layout/layout.component';
 import { LayoutModule } from './layout/layout.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
--- alex/task_1/task1/src/app/layout/layout.module.ts
+++ alex/task_1/task1/src/app/layout/layout.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+import { NgModule  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { HeaderComponent } from '../header/header.component';
